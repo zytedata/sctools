9f8d2ad9fd93e8f82321ae4a9ee7d5b478d0c238
Lucy Wang <wxitb2017@gmail.com>
2021-10-29 11:58:19 +0800

    revert vega versions

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,18 +6,19 @@ set -x
 # ./copy_release_files.sh
 #
 
-owner=${1:-zytedata}
+default_repo=zytedata/sctools
+git remote add upstream https://github.com/${default_repo}.git
+git fetch upstream master
 
-git remote set-url origin https://github.com/$owner/sctools.git
-git fetch origin master
 # create a new clear branch
-
 git add -f docs
 
-git reset --soft origin/master
+git reset --soft upstream/master
 
 git config --global user.email "wxitb2017@gmail.com"
 git config --global user.name "Lucy Wang"
 git commit -a -m 'gh pages update on gh actions'
 
+repo=${GITHUB_REPOSITORY:-$default_repo}
+git remote set-url origin https://github.com/${repo}.git
 git push -f origin HEAD:gh-pages
