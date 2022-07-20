fdd2055c226ca3f7138ddfac9bb1163279d5b78a
Lucy Wang <wxitb2017@gmail.com>
2022-07-20 12:11:06 +0800

    migrate to cypress 10 layout

---
+++ cypress.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  watchForFileChanges: false,
+  projectId: '4hv8zp',
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.ts')(on, config)
+    },
+    baseUrl: 'http://127.0.0.1:3345',
+    specPattern: 'cypress/e2e/**/*_spec.ts',
+  },
+})
